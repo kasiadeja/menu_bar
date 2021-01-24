a7d9638ac84f6032115f67405781c93602223754
KASIA <kasia@MacBook-Pro-KASIA.local>
2021-01-24 12:38:05 +0100

    First commit

---
+++ menu_bar.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("MenuBar");
+    if (x.className === "menu_bar") {
+        x.className += " responsive";
+    }
+    else {
+        x.className = "menu_bar";
+    }
+}
